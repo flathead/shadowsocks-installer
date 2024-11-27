f2199f1a879362e5ddbd7389327efb90abf7c77f
Dmitry Guzeev <dmitry@evrone.com>
2024-11-28 01:48:15 +0300

    language attribute fix

--- installer.sh
+++ installer.sh
@@ -6,17 +6,29 @@ REPO_NAME="shadowsocks-installer"
 APP_DIR="/opt/shadowsocks-vpn"
 SCRIPT_DIR="/usr/local/bin"
 SCRIPT_PATH="${SCRIPT_DIR}/vpn"
-LANGUAGE="en"
+DEFAULT_LANGUAGE="en"
 
 # Prepare the environment and run setup
 install() {
-    sudo mkdir -p "/opt/shadowsocks-vpn"
+    # Создание директории и загрузка файлов
+    sudo mkdir -p "$APP_DIR"
     curl -fsSL "https://raw.githubusercontent.com/${REPO_OWNER}/${REPO_NAME}/refs/heads/master/shadowsocks.sh" -o "${APP_DIR}/shadowsocks.sh"
-    curl -fsSl "https://raw.githubusercontent.com/${REPO_OWNER}/${REPO_NAME}/refs/heads/master/languages.json" -o "${APP_DIR}/languages.json"
+    curl -fsSL "https://raw.githubusercontent.com/${REPO_OWNER}/${REPO_NAME}/refs/heads/master/languages.json" -o "${APP_DIR}/languages.json"
+
+    # Создание ссылки на исполняемый скрипт
     sudo ln -sf "${APP_DIR}/shadowsocks.sh" "${SCRIPT_PATH}" && sudo chmod +x "${APP_DIR}/shadowsocks.sh"
-    vpn setup $LANGUAGE
+
+    # Запуск настройки с выбранным языком
+    "${SCRIPT_PATH}" setup "$1"
 }
 
 # Main script logic
-LANGUAGE=${2:-en}
-install
+# Проверка аргументов
+if [ "$#" -eq 0 ]; then
+    LANGUAGE="$DEFAULT_LANGUAGE"
+else
+    LANGUAGE="$1"
+fi
+
+# Запуск установки
+install "$LANGUAGE"
